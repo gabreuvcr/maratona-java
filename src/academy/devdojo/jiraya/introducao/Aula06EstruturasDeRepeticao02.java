796eb4d8a60aad4d70c036339149dc1d1a36b4d0
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-08-05 02:22:29 -0300

    Aula 031

---
+++ src/academy/devdojo/jiraya/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.jiraya.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 1000; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("");
+    }
+}
