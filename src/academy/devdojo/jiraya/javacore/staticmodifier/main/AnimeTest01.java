fcac84762743fad0357db606ccc6758181f4a98b
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-08-14 05:43:08 -0300

    Removendo erro

--- src/academy/devdojo/jiraya/javacore/staticmodifier/main/AnimeTest01.java
+++ src/academy/devdojo/jiraya/javacore/staticmodifier/main/AnimeTest01.java
@@ -4,9 +4,12 @@ import academy.devdojo.jiraya.javacore.staticmodifier.models.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime1 = new Anime();
-        Anime anime2 = new Anime();
-        Anime anime3 = new Anime();
+        Anime anime1 = new Anime("One Piece");
+        Anime anime2 = new Anime("Boku no Hero");
+        Anime anime3 = new Anime("Naruto");
         
+        System.out.println(anime1.getName());
+        System.out.println(anime2.getName());
+        System.out.println(anime3.getName());
     }
 }
