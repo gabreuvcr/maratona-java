64a69a088edf275b72d303ec0aaee8d7dd0c8a63
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-08-16 23:17:20 -0300

    Aula 076

---
+++ src/academy/devdojo/jiraya/javacore/overriding/main/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.jiraya.javacore.overriding.main;
+
+import academy.devdojo.jiraya.javacore.overriding.models.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("One Piece");
+
+        System.out.println(anime);
+    }
+}
