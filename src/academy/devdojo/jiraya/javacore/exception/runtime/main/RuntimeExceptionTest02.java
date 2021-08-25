bdee0121442e85572cd04207949592dd9165c9b9
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-08-25 00:53:41 -0300

    Aula 098

---
+++ src/academy/devdojo/jiraya/javacore/exception/runtime/main/RuntimeExceptionTest02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.jiraya.javacore.exception.runtime.main;
+
+public class RuntimeExceptionTest02 {
+    public static void main(String[] args) {
+        System.out.println(division(1, 0));
+    }
+
+    /**
+     * 
+     * @param a
+     * @param b Value cannot be 0
+     * @return
+     * @throws IllegalArgumentException if b is 0
+     */
+    private static int division(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by 0");
+        }
+        
+        return a / b;
+    }
+}
