2f605caea3b66dfdcbe3ac4c7fcd49f4bedc2031
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-08-24 23:56:12 -0300

    Removing unused import

--- src/academy/devdojo/jiraya/javacore/polymorphism/main/RepositoryTest01.java
+++ src/academy/devdojo/jiraya/javacore/polymorphism/main/RepositoryTest01.java
@@ -1,7 +1,6 @@
 package academy.devdojo.jiraya.javacore.polymorphism.main;
 
 import academy.devdojo.jiraya.javacore.polymorphism.models.DatabaseRepository;
-import academy.devdojo.jiraya.javacore.polymorphism.models.FileRepository;
 import academy.devdojo.jiraya.javacore.polymorphism.repository.Repository;
 
 public class RepositoryTest01 {
