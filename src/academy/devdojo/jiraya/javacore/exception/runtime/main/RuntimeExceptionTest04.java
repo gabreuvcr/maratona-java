1fda8cbf7b21963316f8f63eb3b20e1ed333c559
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-08-29 02:24:47 -0300

    Aula 102

--- src/academy/devdojo/jiraya/javacore/exception/runtime/main/RuntimeExceptionTest04.java
+++ src/academy/devdojo/jiraya/javacore/exception/runtime/main/RuntimeExceptionTest04.java
@@ -1,36 +1,35 @@
 package academy.devdojo.jiraya.javacore.exception.runtime.main;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
-            throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Inside ArrayIndexOutOfBoundsException");
+            throw new IndexOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.print("Inside ArrayIndexOutOfBoundsException | "); 
+            System.out.print("IllegalArgumentException | ");
+            System.out.println("ArithmeticException");
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Inside IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Inside IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Inside ArithmeticException");
         } catch (RuntimeException e) {
             System.out.println("Inside RuntimeException");
         }
 
         try {
             maybeThrowException();
-        } catch (SQLException e) {
+        } catch (SQLException | FileNotFoundException e) {
             e.printStackTrace();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void maybeThrowException() throws SQLException, FileNotFoundException {
+    private static void maybeThrowException() throws SQLException, FileNotFoundException, IOException {
         return;
     }
 }
