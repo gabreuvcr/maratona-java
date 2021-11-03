44c5e13b05102f201851c62d87334c48bde64195
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-11-03 14:03:40 -0300

    Aula 117

--- src/academy/devdojo/jiraya/javacore/formatting/test/NumberFormatTest02.java
+++ src/academy/devdojo/jiraya/javacore/formatting/test/NumberFormatTest02.java
@@ -4,7 +4,7 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         NumberFormat[] nfa = new NumberFormat[4];
 
@@ -12,21 +12,19 @@ public class NumberFormatTest01 {
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
 
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeBR); 
-        nfa[2] = NumberFormat.getInstance(localeJP);
-        nfa[3] = NumberFormat.getInstance(localeIT);
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeBR); 
+        nfa[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
 
         double value = 1_000.2130;
         for (NumberFormat numberFormat : nfa) {
             System.out.println(numberFormat.format(value));
         }
 
-        String valueString = String.valueOf(value);
-        String valueString2 = "1_000.2130";
+        String valueString = "￥1,000";
         try {
-            System.out.println(nfa[0].parse(valueString));
-            System.out.println(nfa[0].parse(valueString2));
+            System.out.println(nfa[2].parse(valueString));
         } catch (ParseException e) {
             e.printStackTrace();
         } 
